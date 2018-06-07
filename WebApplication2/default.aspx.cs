d8b56904440b5f6ffa978681bde5e718ebfe516f
愷佑 簡 <yui852099@gmail.com>
2018-06-07 12:16:29 +0800

    UserInfo

--- WebApplication2/default.aspx.cs
+++ WebApplication2/default.aspx.cs
@@ -10,8 +10,8 @@ namespace WebApplication2
 {
     public partial class _default : System.Web.UI.Page
     {
-        const string channelAccessToken = "!!!!! 改成自己的ChannelAccessToken !!!!!";
-        const string AdminUserId= "!!!改成你的AdminUserId!!!";
+        const string channelAccessToken = "sQK8v90ohwrqsQ2aB4Uk7VlkXt2eh+gNS5CN0OXXpUWqhCaVUEajfTR/JwZxXNIdtl7Fyj99CKuf27B9Lz5zEth5RtQVD9iVVgZd6VIOxZu0oHm65vat6+fSK4o6zy44Xd4ekzr5O0Hze1Sq2frQpgdB04t89/1O/w1cDnyilFU=";
+        const string AdminUserId= "Ud053c5b30e1bb4c805a2578915204cc7";
 
         protected void Page_Load(object sender, EventArgs e)
         {
